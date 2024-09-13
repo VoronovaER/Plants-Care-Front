8fd19b45f7d064f8cc4edcc61e5e8402ffd11303
VoronovaER <voronova.e.rus@yandex.ru>
2024-09-13 22:24:28 +0300

    Исправлены ошибки с подключением

---
+++ app/src/main/java/com/me/test1/network/ApiClient.java
@@ -0,0 +1,19 @@
+package com.me.test1.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    public static Retrofit retrofit = null;
+
+    static Retrofit getClient(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("http://192.168.134.61:8080/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
